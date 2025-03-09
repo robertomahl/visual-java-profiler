0cdf8813e6bb1a73fe911c4ade564ef2c81ef7ea
Roberto Mahl <robertomahl@live.com>
2025-03-09 20:47:01 -0300

    Add markup adding for previously opened files

---
+++ src/main/java/io/github/robertomahl/visualjavaprofiler/ExampleMarkups.java
@@ -0,0 +1,21 @@
+package io.github.robertomahl.visualjavaprofiler;
+
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.markup.EffectType;
+import com.intellij.openapi.editor.markup.TextAttributes;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiMethod;
+import java.awt.Color;
+import java.awt.Font;
+
+public class ExampleMarkups {
+
+    private void highlightMethod(Project project, PsiMethod method, Editor editor) {
+        TextAttributes attributes1 = new TextAttributes(Color.RED, null, null, EffectType.BOLD_DOTTED_LINE.LINE_UNDERSCORE, Font.BOLD); // Bold Red
+        TextAttributes attributes2 = new TextAttributes(new Color(255, 165, 0), null, null, EffectType.LINE_UNDERSCORE, Font.PLAIN); // Orange
+        TextAttributes attributes3 = new TextAttributes(null, new Color(255, 255, 204), null, null, Font.PLAIN); // Light Yellow Background
+        TextAttributes attributes4 = new TextAttributes(null, null, Color.GRAY, EffectType.WAVE_UNDERSCORE, Font.PLAIN); // Wave Underscore
+        TextAttributes attributes5 = new TextAttributes(null, new Color(255, 165, 0, 100), null, null, Font.PLAIN); // Semi-transparent orange background
+    }
+
+}
--- src/main/java/io/github/robertomahl/visualjavaprofiler/SelfTimeAction.java
+++ src/main/java/io/github/robertomahl/visualjavaprofiler/SelfTimeAction.java
@@ -7,13 +7,12 @@ import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.markup.EffectType;
 import com.intellij.openapi.editor.markup.HighlighterLayer;
 import com.intellij.openapi.editor.markup.HighlighterTargetArea;
-import com.intellij.openapi.editor.markup.MarkupModel;
 import com.intellij.openapi.editor.markup.TextAttributes;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.fileEditor.FileEditorManagerListener;
+import com.intellij.openapi.fileEditor.TextEditor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiClass;
@@ -22,20 +21,19 @@ import com.intellij.psi.PsiJavaFile;
 import com.intellij.psi.PsiManager;
 import com.intellij.psi.PsiMethod;
 import com.intellij.psi.PsiParameter;
-import com.intellij.psi.PsiSubstitutor;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.messages.MessageBusConnection;
 import java.awt.Color;
 import java.awt.Font;
+import java.util.List;
 import org.jetbrains.annotations.NotNull;
 
 public class SelfTimeAction extends AnAction {
 
-    //TODO: consider finding approach that does no require the action to be run first: "Use Startup Activity if it should always run when the project opens"?
-    //TODO: once the action is performed, also execute for already opened files
-    //TODO: should I stop the highlighting if the method's been changed since the profiling execution?
+    //TODO: stop the highlighting if the method's been changed since the profiling execution?
+    //TODO: add start/stop logic for the action
 
-    //TODO: add start/stop logic for actions
+    //TODO: consider finding approach that does no require the action to be run first: "Use Startup Activity if it should always run when the project opens"?
 
     private static final String TARGET_METHOD_SIGNATURE = "com.minguard.service.impl.StatusServiceImpl.findAll()";
 
@@ -44,10 +42,7 @@ public class SelfTimeAction extends AnAction {
         Project project = e.getProject();
         Editor editor = e.getData(CommonDataKeys.EDITOR);
 
-        //TODO: Drop this requirement
-        PsiFile psiFile = e.getData(CommonDataKeys.PSI_FILE);
-
-        e.getPresentation().setEnabled(project != null && editor != null && psiFile != null);
+        e.getPresentation().setEnabled(project != null && editor != null);
     }
 
     @Override
@@ -59,36 +54,60 @@ public class SelfTimeAction extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent anActionEvent) {
         Project project = anActionEvent.getProject();
-        if (project != null) {
-            registerFileOpenListener(project);
+        if (project == null) {
+            return;
         }
 
+        registerFileOpenListener(project);
+        applyToAllOpenFiles(project);
     }
 
-    public void registerFileOpenListener(@NotNull Project project) {
+    private void registerFileOpenListener(Project project) {
         MessageBusConnection connection = project.getMessageBus().connect();
         connection.subscribe(FileEditorManagerListener.FILE_EDITOR_MANAGER, new FileEditorManagerListener() {
             @Override
             public void fileOpened(@NotNull FileEditorManager source, @NotNull VirtualFile file) {
                 //Moving operation to BGT, since Psi reading is costly and shouldn't be done in EDT
                 ApplicationManager.getApplication().executeOnPooledThread(() -> {
-                    PsiManager psiManager = PsiManager.getInstance(project);
-
-                    PsiFile psiFile = ReadAction.compute(() -> psiManager.findFile(file));
-                    if (psiFile instanceof PsiJavaFile
-                            && psiFile.isValid()) {
-                        ReadAction.run(() -> highlightTargetMethod((PsiJavaFile) psiFile, project));
-                    }
+                    applyToFile(project, file);
                 });
             }
         });
     }
 
-    private void highlightTargetMethod(PsiJavaFile psiFile, Project project) {
+    private void applyToAllOpenFiles(Project project) {
+        FileEditorManager fileEditorManager = FileEditorManager.getInstance(project);
+
+        VirtualFile[] openFiles = fileEditorManager.getOpenFiles();
+        for (VirtualFile virtualFile : openFiles) {
+            applyToFile(project, virtualFile);
+        }
+    }
+
+    private void applyToFile(Project project, VirtualFile virtualFile) {
+        PsiFile psiFile = ReadAction.compute(() -> PsiManager.getInstance(project).findFile(virtualFile));
+        if (psiFile instanceof PsiJavaFile
+                && psiFile.isValid()) {
+
+            // FileEditor instances can be non-textual, so we need to get only the textual ones (Editor)
+            List<Editor> editors = FileEditorManager.getInstance(project).getEditorList(virtualFile)
+                    .stream()
+                    .filter(TextEditor.class::isInstance)
+                    .map(TextEditor.class::cast)
+                    .map(TextEditor::getEditor)
+                    .toList();
+
+            if (!editors.isEmpty())
+                ReadAction.run(() -> highlightTargetMethod((PsiJavaFile) psiFile, project, editors));
+        }
+
+    }
+
+    private void highlightTargetMethod(PsiJavaFile psiFile, Project project, List<Editor> editors) {
         for (PsiMethod method : PsiTreeUtil.findChildrenOfType(psiFile, PsiMethod.class)) {
             String methodSignature = getMethodSignature(method);
             if (TARGET_METHOD_SIGNATURE.equals(methodSignature)) {
-                highlightMethod(project, method);
+                editors.forEach(editor -> highlightMethod(project, method, editor));
             }
         }
     }
@@ -117,17 +136,14 @@ public class SelfTimeAction extends AnAction {
         return signature.toString();
     }
 
-    private void highlightMethod(Project project, PsiMethod method) {
-        Editor editor = FileEditorManager.getInstance(project).getSelectedTextEditor();
-        if (editor == null) return;
-
-        //TODO: improve highlighting so it is applied to all text within the method
-        MarkupModel markupModel = editor.getMarkupModel();
-        TextAttributes attributes = new TextAttributes(Color.RED, null, null, EffectType.LINE_UNDERSCORE, Font.BOLD);
+    private void highlightMethod(Project project, PsiMethod method, Editor editor) {
+        //TODO: change to JBColor, with dark respective color
+        TextAttributes attributes = new TextAttributes(null, new Color(255, 165, 0, 100), null, null, Font.PLAIN); // Semi-transparent orange background
 
         int startOffset = method.getTextRange().getStartOffset();
         int endOffset = method.getTextRange().getEndOffset();
-        markupModel.addRangeHighlighter(startOffset, endOffset, HighlighterLayer.ADDITIONAL_SYNTAX, attributes, HighlighterTargetArea.EXACT_RANGE);
+
+        editor.getMarkupModel().addRangeHighlighter(startOffset, endOffset, HighlighterLayer.LAST, attributes, HighlighterTargetArea.EXACT_RANGE);
     }
 
 }
