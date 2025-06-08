ab8a7c67c174344e1f5d5e4047ebf11f35c5417f
Roberto Mahl <robertomahl@live.com>
2025-06-08 17:06:59 -0300

    Update plug-in description

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,34 @@
 plugins {
-  id("java")
-  id("org.jetbrains.kotlin.jvm") version "1.9.25"
-  id("org.jetbrains.intellij") version "1.17.4"
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.9.25"
+    id("org.jetbrains.intellij") version "1.17.4"
 }
 
 group = "io.github.robertomahl"
-version = "1.0.3"
+version = "1.0.4"
 
 repositories {
-  mavenCentral()
+    mavenCentral()
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-  version.set("2024.1.7")
-  type.set("IC")
-  plugins.set(listOf("com.intellij.java"))
+    version.set("2024.1.7")
+    type.set("IC")
+    plugins.set(listOf("com.intellij.java"))
 }
 
 tasks {
-  // Set the JVM compatibility versions
-  withType<JavaCompile> {
-    sourceCompatibility = "17"
-    targetCompatibility = "17"
-  }
-  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
-  }
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
 
-  patchPluginXml {
-    sinceBuild.set("232")
-    untilBuild.set("252.*")
-  }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
 
-  signPlugin {
-    certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-    privateKey.set(System.getenv("PRIVATE_KEY"))
-    password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-  }
-
-  publishPlugin {
-    token.set(System.getenv("PUBLISH_TOKEN"))
-  }
+    patchPluginXml {
+        sinceBuild.set("232")
+        untilBuild.set("252.*")
+    }
 }
